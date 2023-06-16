cddfaee8983aa87a702e78d279376cf76d3ceebf
zyf <2095026158@qq.com>
2023-06-16 16:31:01 +0800

    完成了初始目录的调整

--- src/main.js
+++ src/main.js
@@ -2,6 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// 引入全局样式
+import './styles/index.less'
+// 引入vant组件库
+import Vant from 'vant'
+import 'vant/lib/index.css'
+Vue.use(Vant)
 
 Vue.config.productionTip = false
 
